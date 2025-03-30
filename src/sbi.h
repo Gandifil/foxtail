78b5c1f83e26658dfabf0f0cd793159def77ea48
Gandifil <gandifilmk@gmail.com>
2025-03-30 19:37:27 +0300

    Add sbi module

---
+++ src/sbi.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define SBI_CONSOLE_PUTCHAR 1
+
+struct sbiret {
+    long error;
+    long value;
+};
+
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid);
+
+inline struct sbiret sbi_call1(long arg0, long eid){
+    return sbi_call(arg0, 0, 0, 0, 0, 0, 0, eid);
+}
