b32b8154df9d07520efdc7760f2a7122b0a53ab7
Gandifil <gandifilmk@gmail.com>
2025-03-30 19:24:02 +0300

    Move all source files to src folder

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@ QEMU=qemu-system-riscv32
 # Путь к clang и его флагам
 CC=clang  # Для Ubuntu: используйте CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+SOURCE_FILES="src/kernel.c"
 
 # Сборка ядра
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
-
+    $SOURCE_FILES
 
 # run QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
