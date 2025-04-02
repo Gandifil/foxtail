2a8d72ace258dbdfddb9396fcd62a67fdd21401b
Gandifil <gandifilmk@gmail.com>
2025-04-02 21:50:37 +0300

    Add processes

--- src/kernel.c
+++ src/kernel.c
@@ -93,14 +93,151 @@ void handle_trap(struct trap_frame *f) {
     PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
 }
 
+struct process procs[PROCS_MAX]; // Все блоки управления процессами.
+
+struct process *create_process(uint32_t pc) {
+    // Поиск блока управления неиспользуемого процесса. 
+    struct process *proc = NULL;
+    int i;
+    for (i = 0; i < PROCS_MAX; i++) {
+        if (procs[i].state == PROC_UNUSED) {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+        PANIC("no free process slots");
+
+    // Запись в стек значений регистров, сохранённых вызываемым кодом. Эти значения будут восстановлены при первом переключении контекста функцией switch_context.
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0;                      // s11
+    *--sp = 0;                      // s10
+    *--sp = 0;                      // s9
+    *--sp = 0;                      // s8
+    *--sp = 0;                      // s7
+    *--sp = 0;                      // s6
+    *--sp = 0;                      // s5
+    *--sp = 0;                      // s4
+    *--sp = 0;                      // s3
+    *--sp = 0;                      // s2
+    *--sp = 0;                      // s1
+    *--sp = 0;                      // s0
+    *--sp = (uint32_t) pc;          // ra
+
+    // Инициализация полей.
+    proc->pid = i + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+}
+
+__attribute__((naked)) 
+void switch_context(uint32_t *prev_sp, uint32_t *next_sp) {
+    __asm__ __volatile__(
+        // Запись сохранённых вызываемым кодом значений регистров в стек текущего процесса.
+        "addi sp, sp, -13 * 4\n" // Аллокация пространства стека для 13 4-байтовых регистров.
+        "sw ra,  0  * 4(sp)\n"   // Запись только сохранённых вызываемым кодом значений регистров.
+        "sw s0,  1  * 4(sp)\n"
+        "sw s1,  2  * 4(sp)\n"
+        "sw s2,  3  * 4(sp)\n"
+        "sw s3,  4  * 4(sp)\n"
+        "sw s4,  5  * 4(sp)\n"
+        "sw s5,  6  * 4(sp)\n"
+        "sw s6,  7  * 4(sp)\n"
+        "sw s7,  8  * 4(sp)\n"
+        "sw s8,  9  * 4(sp)\n"
+        "sw s9,  10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+
+        // Переключение указателя стека.
+        "sw sp, (a0)\n"         // *prev_sp = sp;
+        "lw sp, (a1)\n"         // Переключение указателя стека (sp) сюда.
+
+        // Восстановление сохранённых вызываемым кодом значений регистров из стека следующего процесса.
+        "lw ra,  0  * 4(sp)\n"  // Восстановление только сохранённых вызываемым кодом значений регистров.
+        "lw s0,  1  * 4(sp)\n"
+        "lw s1,  2  * 4(sp)\n"
+        "lw s2,  3  * 4(sp)\n"
+        "lw s3,  4  * 4(sp)\n"
+        "lw s4,  5  * 4(sp)\n"
+        "lw s5,  6  * 4(sp)\n"
+        "lw s6,  7  * 4(sp)\n"
+        "lw s7,  8  * 4(sp)\n"
+        "lw s8,  9  * 4(sp)\n"
+        "lw s9,  10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n"  // Извлекли из стека 13 4-байтовых регистров.
+        "ret\n"
+    );
+}
+
+struct process *current_proc; // Выполняющийся в текущий момент процесс.
+struct process *idle_proc;    // Бездействующий процесс.
+
+void yield(void) {
+    // Поиск готового к выполнению процесса.
+    struct process *next = idle_proc;
+    for (int i = 0; i < PROCS_MAX; i++) {
+        struct process *proc = &procs[(current_proc->pid + i) % PROCS_MAX];
+        if (proc->state == PROC_RUNNABLE && proc->pid > 0) {
+            next = proc;
+            break;
+        }
+    }
+
+    // Если за исключением текущего процесса других готовых к выполнению процессов нет, произвести возврат и продолжить обработку.
+    if (next == current_proc)
+        return;
+
+    // Переключение контекста.
+    struct process *prev = current_proc;
+    current_proc = next;
+    switch_context(&prev->sp, &next->sp);
+}
+
+void delay(void) {
+    for (int i = 0; i < 30000000; i++)
+        __asm__ __volatile__("nop"); // do nothing
+}
+
+struct process *proc_a;
+struct process *proc_b;
+
+void proc_a_entry(void) {
+    printf("starting process A\n");
+    while (1) {
+        putchar('A');
+        yield();
+        delay();
+    }
+}
+
+void proc_b_entry(void) {
+    printf("starting process B\n");
+    while (1) {
+        putchar('B');
+        yield();
+        delay();
+    }
+}
+
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
-    WRITE_CSR(stvec, (uint32_t) kernel_entry); 
-    __asm__ __volatile__("unimp"); 
 
-    for (;;) {
-        __asm__ __volatile__("wfi");
-    }
+    WRITE_CSR(stvec, (uint32_t) kernel_entry); 
+
+    idle_proc = create_process((uint32_t) NULL);
+    idle_proc->pid = -1; // Бездействует.
+    current_proc = idle_proc;
+
+    proc_a = create_process((uint32_t) proc_a_entry);
+    proc_b = create_process((uint32_t) proc_b_entry);
+
+    yield();
+    PANIC("switched to idle process");
 }
 
 __attribute__((section(".text.boot")))
--- src/kernel.h
+++ src/kernel.h
@@ -53,3 +53,15 @@ struct trap_frame {
         printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
         while (1) {}                                                           \
     } while (0)
+
+#define PROCS_MAX 8       //Максимальное число процессов.
+
+#define PROC_UNUSED   0   // Неиспользуемый процесс.
+#define PROC_RUNNABLE 1   // Готовый к выполнению процесс.
+
+struct process {
+    int pid;             // ID процесса.
+    int state;           // Состояние процесса: PROC_UNUSED или PROC_RUNNABLE
+    vaddr_t sp;          // Указатель стека.
+    uint8_t stack[8192]; // Стек ядра.
+};
